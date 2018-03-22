2fa1f03b80a388263062d9cc29799de099273416
lujing8023 <1042351095@qq.com>
2018-03-22 11:30:11 +0800

    新修改复制节点名字

--- assets/Script/Game.js
+++ assets/Script/Game.js
@@ -9,16 +9,15 @@ cc.Class({
         ndListParTwo: cc.Node,
         ndListTwo: cc.Node,
         numRem: 0,
-        // //盘子图片资源
         picFram: {
             type: cc.SpriteFrame,
             default: [],
         }
     },
-    onLoad: function() {
+    onLoad: function () {
         this.upUi();
     },
-    onButtonClick: function(event, cb) {
+    onButtonClick: function (event, cb) {
         switch (cb) {
             case "back":
                 this.ndBox.active = true;
@@ -31,37 +30,34 @@ cc.Class({
                 break;
             case "next":
                 if (this.numRem == 1) {
-                    this.ndListPar.active = false;
-                    this.ndListParTwo.active = true;
                     this.numRem = 2;
                     this.destroyFunc();
+                    this.ndListPar.active = false;
+                    this.ndListParTwo.active = true;
                 } else {
+                    this.numRem = 1;
                     this.ndListPar.active = true;
                     this.ndListParTwo.active = false;
-                    this.numRem = 1;
                 }
                 break;
         }
     },
-    upUi: function() {
+    upUi: function () {
         this.numRem = 1;
         _.times(this.picFram.length, (i) => {
             let node = cc.instantiate(this.ndP);
             node.SpriteFrame = i;
-            // node.name = i
+            node.name = i + " ";
+            cc.log("name", node.name)
             this.ndList.addChild(node);
         })
     },
-    destroyFunc: function() {
-        if (this.numRem == 2) {
-            while (this.ndListPar.children.length > 0) {
-                this.ndList.children[0].removeFromParent()
-            }
-        } else {
-            // _.times(this.ndListTwo.children.length,()=>{
-            //     let node = cc.find("P",node)
-            //     node.destroy();
-            // })
+    destroyFunc: function () {
+        while (this.ndList.children.length > 0) {
+            this.ndList.children[0].removeFromParent()
         }
+    },
+    eventTarget: function () {
+
     },
 });
